2da19d484a19c3c8fb4da58f51946220eba8a200
Fadloovich <arabea169@gmail.com>
2023-02-13 17:25:54 +0200

    Added List Component

--- src/App.js
+++ src/App.js
@@ -10,12 +10,23 @@ function App() {
   const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
   const handleSubmit = (e) => {
     e.preventDefault();
+    if (!name) {
+      // Display Alert
+    } else if (name && isEditing) {
+      // Deal With Edit
+    } else {
+      // Show alert
+      const newItem = { id: new Date().getTime().toString(), title: name };
+      setList([...list, newItem]);
+      setName("");
+    }
+
     console.log("FFFFF");
   };
   return (
     <section className="section-center">
       <form onSubmit={handleSubmit} className="grocery-form">
-        {alert.show && <Alert />}
+        {alert.show && <Alert {...alert} />}
         <h3>Grocery Bud</h3>
         <div className="form-control">
           <input
@@ -32,10 +43,12 @@ function App() {
           </button>
         </div>
       </form>
-      <div className="grocery-container">
-        <List />
-        <button className="clear-btn">Clear Items</button>
-      </div>
+      {list.length > 0 && (
+        <div className="grocery-container">
+          <List items={list} />
+          <button className="clear-btn">Clear Items</button>
+        </div>
+      )}
     </section>
   );
 }
